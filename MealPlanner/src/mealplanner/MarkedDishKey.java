bd9d5d8e641d7765141e75613c52f75f2c76d20b
jamie <jjablin@mahna.(none)>
2012-04-26 13:34:50 -0400

    Class MarkedDishKey now implements Comparable interface.

--- MealPlanner/src/mealplanner/MarkedDishKey.java
+++ MealPlanner/src/mealplanner/MarkedDishKey.java
@@ -9,15 +9,25 @@ package mealplanner;
  *
  * @author seadams
  */
-public class MarkedDishKey {
+public class MarkedDishKey implements Comparable {
 
-    private Location _location;
-    private Meal _meal;
-    private Day _day;
+  private Location _location;
+  private Meal _meal;
+  private Day _day;
 
-    public MarkedDishKey(Location location, Meal meal, Day day) {
-        _location = location;
-        _meal = meal;
-        _day = day;
-    }
+  public MarkedDishKey(Location location, Meal meal, Day day) {
+    _location = location;
+    _meal = meal;
+    _day = day;
+  }
+
+  public int compareTo(Object obj) {
+    String objStr = new String(((MarkedDishKey)obj)._location.toString() +
+                               ((MarkedDishKey)obj)._meal.toString() +
+                               ((MarkedDishKey)obj)._day.toString());
+    String thisStr = new String(_location.toString() +
+                                _meal.toString() +
+                                _day.toString());
+    return thisStr.compareTo(objStr);
+  }
 }
